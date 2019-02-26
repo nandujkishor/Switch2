cd6cedf5c799f0cda7b3fa21a70b3a0d3eacca69
Aswanth <aswanth@am.students.amrita.edu>
2019-02-26 23:21:33 +0530

    Changes

--- app/pay.py
+++ app/pay.py
@@ -22,7 +22,7 @@ iv = "3564234432724374"
 pay = Blueprint('pay', __name__)
 
 class AESCipher(object):
-    def __init__(self, key):
+    def __init__(self):
         self.bs = 16
         self.cipher = AES.new(key, AES.MODE_CBC, iv)
 
@@ -60,13 +60,13 @@ def payauth():
 
 @pay.route('/testing/', methods=['POST', 'GET'])
 def payment():
-    plaintext = "transactionId=VIDYUTTEST10|amount=1|purpose=VIDYUT19TEST|currency=inr"
+    plaintext = "transactionId=VIDYUTTEST12|amount=1|purpose=VIDYUT19TEST|currency=inr"
     result = hashlib.md5(plaintext.encode())
     result = result.hexdigest()
     print("md5",result)
     pwc = plaintext + "|checkSum=" + result
     print("before aes",pwc)
-    cipher = AESCipher(key)
+    cipher = AESCipher()
     encd = cipher.encrypt(pwc)
     print("after aes", encd)
     return encd
@@ -80,3 +80,13 @@ def callback():
 @pay.route('/check/')
 def check_pay():
     return render_template('payment.html')
+@pay.route('/hehe')
+def hehe():
+    check = "XGg+vDd6ejtOixqgRU7tErxv+VWbfDcOZofQ6VFcCRziU5GfgTVeDu61PFK2qMzcW83ZJx/zFd2ifRrDgsoU0NxolcDY9hoh5BQRkAamov+ERmb4XlFURCn0OamBb+9qXoQ7+OEySS7zumVRIJ8bJAB8vSZm0RXlsjYwkaX97Ua7cofDvtGK6+Xrdi8/FWIWYa8a4SK7Bx1sGZSu9DEieSn5tHCwD9bPHBIjtaY+7jxy1sTS6MMAxpTlnGUHikev"
+    cipher = AESCipher()
+    data = cipher.decrypt(check)
+    v = data.split('|')
+    req = v[5].split('=')
+    print(req[1])
+    # print(v)
+    return data
